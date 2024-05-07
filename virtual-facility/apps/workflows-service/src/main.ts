bb020f73886e33805a481a6fb1d3d43dffd851a9
CristianDI <git>
2024-05-06 21:56:22 -0500

    Converting from HTTP to NATS

--- virtual-facility/apps/workflows-service/src/main.ts
+++ virtual-facility/apps/workflows-service/src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { WorkflowsServiceModule } from './workflows-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   console.log('Starting Workflows Service');
   const app = await NestFactory.create(WorkflowsServiceModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  console.log('Connecting to NATS', process.env.NATS_URL);
+
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: [process.env.NATS_URL],
+      },
+    },
+    { inheritAppConfig: true },
+  );
+
+  await app.startAllMicroservices();
+
   await app.listen(3001);
 }
 bootstrap();
